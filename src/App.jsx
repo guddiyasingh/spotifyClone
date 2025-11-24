3cd0204b4903f5adfee88b90e4cb7f2a2000aebd
Guddiya Singh <guddiyasingh1847@gmail.com>
2025-11-24 13:43:41 +0530

    add some css

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,21 @@ function App() {
   return (
     <>
      <div className="container flex">
-      <div className='left border'>
-        <div className="home"></div>
-        <div className="library"></div>
+      <div className='left border bg-black'>
+        <div className="home">
+          <div className="logo">
+            <img className="invert"src="spotify.png" alt="" />spotify
+          </div>
+          <ul>
+            <li>
+              <img className='invert' src="home.svg" alt="home" /> Home
+            </li>
+            <li><img className="invert"src="search.svg" alt="search" />Search</li>
+          </ul>
+        </div>
+        <div className="library bg-black"></div>
       </div>
-      <div className='right border'>I miss you shivam</div>
+      <div className='right border'></div>
      </div>
     </>
   )
