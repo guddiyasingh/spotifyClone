ee7d53ea8fb0aa78f6673ce00b153f994e802156
Guddiya Singh <guddiyasingh1847@gmail.com>
2025-12-03 00:31:50 +0530

    add somgs.js file in src folder

---
+++ src/songs.js
@@ -0,0 +1,27 @@
+export const songs = [
+  {
+    title: "Carefree",
+    artist: "Kevin MacLeod",
+    url: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/no_curator/Kevin_MacLeod/Music/Kevin_MacLeod_-_Carefree.mp3"
+  },
+  {
+    title: "Sneaky Snitch",
+    artist: "Kevin MacLeod",
+    url: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/No%20Curator/Kevin_MacLeod/Sneaky%20Snitch/Kevin_MacLeod_-_Sneaky_Snitch.mp3"
+  },
+  {
+    title: "Sunny Morning",
+    artist: "Music For Videos",
+    url: "https://cdn.pixabay.com/download/audio/2022/03/15/audio_9287c01e46.mp3"
+  },
+  {
+    title: "Epic Motivation",
+    artist: "Alexi Action",
+    url: "https://cdn.pixabay.com/download/audio/2021/09/20/audio_68f4a1c9d6.mp3"
+  },
+  {
+    title: "LoFi Study",
+    artist: "FASSounds",
+    url: "https://cdn.pixabay.com/download/audio/2022/02/23/audio_4b77fa1f13.mp3"
+  }
+];
